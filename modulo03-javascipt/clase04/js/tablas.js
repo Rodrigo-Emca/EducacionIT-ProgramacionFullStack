399e8271c0b7267ee9d72fcd45801053dc8af0b0
Rodrigo <contactorodrigocabrera@gmail.com>
2023-07-31 16:01:20 -0300

    clase 04 | tablas

---
+++ modulo03-javascipt/clase04/js/tablas.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector('title').textContent)
+
+const solTorres = {nombre: 'Sol', apellido: 'Torres'}
+const leilaRomero = {nombre: 'Leila' , apellido: 'Romero'}
+const francoCarril = {nombre: 'Franco', apellido: 'Carril'}
+const jeronimoVega = {nombre: 'Jeronimo', apellido: 'Vega'}
+
+const arrayPersonas = [solTorres, leilaRomero, francoCarril, jeronimoVega]
+console.log(arrayPersonas)
