d70512b38232024f715c334c73ad074cfbde87a3
Rodrigo <contactorodrigocabrera@gmail.com>
2023-07-31 16:05:18 -0300

    clase 04 | tablas

--- modulo03-javascipt/clase04/js/tablas.js
+++ modulo03-javascipt/clase04/js/tablas.js
@@ -6,4 +6,8 @@ const francoCarril = {nombre: 'Franco', apellido: 'Carril'}
 const jeronimoVega = {nombre: 'Jeronimo', apellido: 'Vega'}
 
 const arrayPersonas = [solTorres, leilaRomero, francoCarril, jeronimoVega]
-console.log(arrayPersonas)
+
+console.log(arrayPersonas) //Puedo acceder al array completo
+console.log(arrayPersonas[0]) //Acceder a un objeto dentro del array
+console.log(arrayPersonas[1].nombre) //Acceder a alguna propiedad de un objeto particular
+console.log(arrayPersonas[2].apellido)
